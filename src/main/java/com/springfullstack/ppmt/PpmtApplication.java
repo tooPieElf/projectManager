f852339cee3cfab3adde503efd2ed7fa8eb30cf4
tooPieElf <ndimnezechukwudi@gmail.com>
2019-12-08 17:07:13 +0100

    Initial commit

---
+++ src/main/java/com/springfullstack/ppmt/PpmtApplication.java
@@ -0,0 +1,13 @@
+package com.springfullstack.ppmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PpmtApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/springfullstack/ppmt/PpmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springfullstack.ppmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
