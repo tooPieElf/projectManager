ac47bdd62322ffe8be0125426fa68f9795a84ac7
tooPieElf <ndimnezechukwudi@gmail.com>
2020-04-21 19:28:58 +0100

    add BacklogRepository and ProjectTaskRepository

---
+++ src/main/java/com/springfullstack/ppmt/repository/BacklogRepository.java
@@ -0,0 +1,9 @@
+package com.springfullstack.ppmt.repository;
+
+import com.springfullstack.ppmt.domain.BackLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BacklogRepository extends JpaRepository<BackLog, Long> {
+
+
+}
---
+++ src/main/java/com/springfullstack/ppmt/repository/ProjectTaskRepository.java
@@ -0,0 +1,8 @@
+package com.springfullstack.ppmt.repository;
+
+import com.springfullstack.ppmt.domain.ProjectTask;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectTaskRepository extends JpaRepository<ProjectTask, Long> {
+
+}
