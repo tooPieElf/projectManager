63c866f27e464445bf4802863f131a0aa7f4a6ea
tooPieElf <ndimnezechukwudi@gmail.com>
2020-04-24 21:49:25 +0100

    add projectRepository

--- src/main/java/com/springfullstack/ppmt/controller/BackLogController.java
+++ src/main/java/com/springfullstack/ppmt/controller/BackLogController.java
@@ -3,12 +3,14 @@ package com.springfullstack.ppmt.controller;
 import com.springfullstack.ppmt.domain.ProjectTask;
 import com.springfullstack.ppmt.service.MapValidationErrorService;
 import com.springfullstack.ppmt.service.ProjectTaskService;
+import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,6 +37,10 @@ public class BackLogController {
     }
     ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask);
     return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
+  }
+  @GetMapping("/{backlog_id}")
+  public ResponseEntity<List<ProjectTask>> getProjectBackLog(@PathVariable String backlog_id){
+    return new ResponseEntity<List<ProjectTask>>()
   }
 
 
--- src/main/java/com/springfullstack/ppmt/repository/ProjectTaskRepository.java
+++ src/main/java/com/springfullstack/ppmt/repository/ProjectTaskRepository.java
@@ -1,8 +1,10 @@
 package com.springfullstack.ppmt.repository;
 
 import com.springfullstack.ppmt.domain.ProjectTask;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectTaskRepository extends JpaRepository<ProjectTask, Long> {
 
+  List<ProjectTask> findByProjectIdentifierOrderByPriority(String id);
 }
