857b74ee595aaba145a8396e4cdbb6f7f1a7139d
tooPieElf <ndimnezechukwudi@gmail.com>
2020-04-24 21:41:22 +0100

    optimized project call

--- src/main/java/com/springfullstack/ppmt/domain/Project.java
+++ src/main/java/com/springfullstack/ppmt/domain/Project.java
@@ -1,6 +1,7 @@
 package com.springfullstack.ppmt.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.Date;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -43,6 +44,7 @@ public class Project {
   private Date updated_At;
 
   @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "project")
+  @JsonIgnore
   private BackLog backLog;
 
   @PrePersist
