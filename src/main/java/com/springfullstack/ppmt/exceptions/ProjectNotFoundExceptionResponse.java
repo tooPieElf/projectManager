8ecc099120d31bbe6816c045af64fa954d343936
tooPieElf <ndimnezechukwudi@gmail.com>
2020-04-29 16:06:25 +0100

    add ProjectNotFoundException

--- src/main/java/com/springfullstack/ppmt/exceptions/ProjectNotFoundExceptionResponse.java
+++ src/main/java/com/springfullstack/ppmt/exceptions/ProjectNotFoundExceptionResponse.java
@@ -1,5 +1,17 @@
 package com.springfullstack.ppmt.exceptions;
 
 public class ProjectNotFoundExceptionResponse {
+private String ProjectNotFound;
 
+  public ProjectNotFoundExceptionResponse(String projectNotFound) {
+    ProjectNotFound = projectNotFound;
+  }
+
+  public String getProjectNotFound() {
+    return ProjectNotFound;
+  }
+
+  public void setProjectNotFound(String projectNotFound) {
+    ProjectNotFound = projectNotFound;
+  }
 }
